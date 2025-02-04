c311e40e30dd251c164bd746d86a5956acb23982
najeesimmons <najee.bryant.simmons@gmail.com>
2025-02-03 23:15:50 -0500

    add meta tags + descrip

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,6 +4,12 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <title>Najee Simmons Portfolio</title>
+      <meta
+        name="description"
+        content="Najee Simmons is a software engineer specializing in building scalable, feature-rich web applications. Explore his portfolio to see projects, skills, and experience."
+      />
+      <meta property="og:title" content="Najee Simmons Resume" />
       <body className="antialiased">
         <Main />
         <NextScript />
