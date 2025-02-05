c57c62d2de3157eef5505a8771d0e1f51406639a
najeesimmons <najee.bryant.simmons@gmail.com>
2025-02-05 17:59:01 -0500

    semantic html

--- src/components/Section/Section.js
+++ src/components/Section/Section.js
@@ -4,12 +4,12 @@ function Section({ className, children, title, id, bgcolor }) {
   return (
     <section id={id} className={`mx-auto max-w-3xl ${className}`}>
       {title && (
-        <h3
+        <h2
           className={`inline-block tracking-widest font-semibold mt-8 mb-8 px-4 dark:text-black`}
           style={{ backgroundColor: bgcolor }}
         >
           {title}
-        </h3>
+        </h2>
       )}
       <div>{children}</div>
     </section>
