10dcf00da15811396ce2772e1f931ac2a8ff13b4
najeesimmons <najee.bryant.simmons@gmail.com>
2025-02-05 18:18:01 -0500

    drkmode

--- src/components/Buttons/CTAButton.js
+++ src/components/Buttons/CTAButton.js
@@ -3,7 +3,7 @@ import React from "react";
 function CTAButton({ href, label, className, isLinkOut }) {
   const content = (
     <button
-      className={`max-w-md bg-[#ef9218] text-gray-800 py-3 px-4 w-48 text-xs font-semibold tracking-widest shadow-2xl transition duration-700 ease-in-out hover:bg-[#ef9218cc] ${className}`}
+      className={`max-w-md bg-[#ef9218] text-black py-3 px-4 w-48 text-xs font-semibold tracking-widest shadow-2xl transition duration-700 ease-in-out hover:bg-[#ef9218cc] ${className}`}
     >
       {label}
     </button>
