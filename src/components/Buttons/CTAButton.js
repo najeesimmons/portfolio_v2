9fc4f50899e6399368258120b17ce6dca3e450d1
najeesimmons <najee.bryant.simmons@gmail.com>
2025-02-06 18:28:44 -0500

    cta button now a tag, semantic

--- src/components/Buttons/CTAButton.js
+++ src/components/Buttons/CTAButton.js
@@ -1,25 +1,16 @@
 import React from "react";
 
-function CTAButton({ href, label, className, isLinkOut }) {
-  const content = (
-    <button
-      className={`max-w-md bg-[#ef9218] text-black py-3 px-4 w-48 text-xs font-semibold tracking-widest shadow-2xl transition duration-700 ease-in-out hover:bg-[#ef9218cc] ${className}`}
+function CTAButton({ href, label, className = "", isLinkOut }) {
+  return (
+    <a
+      href={href}
+      target={isLinkOut ? "_blank" : undefined}
+      rel={isLinkOut ? "noopener noreferrer" : undefined}
+      className={`max-w-md bg-[#ef9218] text-black py-3 px-4 w-48 text-sm font-semibold tracking-widest shadow-2xl transition duration-700 ease-in-out hover:bg-[#ef9218cc] flex items-center justify-center ${className}`}
     >
       {label}
-    </button>
+    </a>
   );
-
-  if (isLinkOut) {
-    return (
-      <a href={href} target="_blank" rel="noopener noreferrer">
-        {content}
-      </a>
-    );
-  } else {
-    return <a href={href}>{content}</a>;
-  }
-
-  return content;
 }
 
 export default CTAButton;
